acbe9337fd1f0afd076e891daeb1918dd8718809
Raesgithub <productraes@gmail.com>
2023-10-12 16:07:13 +0330

    ffff

---
+++ BlazorStudent/Client/Helpers/SweetAlert.cs
@@ -0,0 +1,47 @@
+﻿namespace BlazorStudent.Client.Helpers
+{
+    public static class SweetAlert
+    {
+        public static string icon_success = "success";
+        public static string icon_error = "error";
+        public static string icon_warning = "warning";
+        public static string icon_info = "info";
+        public static string icon_question = "question";
+        public static string position_top_end = "top-end";
+        public static string position_top_start = "top-start";
+        public static string position_top = "top";
+        public static string position_center = "center";
+        public static string position_center_start = "center-start";
+        public static string position_center_end = "center-end";
+        public static string position_bottom = "bottom";
+        public static string position_bottom_start = "bottom-start";
+        public static string position_bottom_end = "bottom-end";
+
+        public static string Toast(string title, string icon = "success", string position = "top-end", int timer = 1500)
+        {
+            //icon: 'success',
+            return @"Swal.fire({
+                              position: '{position}',
+                              icon: '{icon}',
+                              title: ' {title} ',
+                              showConfirmButton: false,
+                              timer: {timer}
+                            })
+                         ".Replace("{title}", title).Replace("{timer}", timer.ToString()).Replace("{icon}", icon).Replace("{position}", position);
+        }
+        public static string Modal(string title, string text, string icon = "success")
+        {
+            return @"Swal.fire({
+                          icon: '{icon}',
+                          title: '{title}',
+                          text: '{text}!'
+                        })
+                      ".Replace("{title}", title).Replace("{text}", text).Replace("{icon}", icon);
+        }
+
+
+
+
+    }
+
+}
--- BlazorStudent/Client/Program.cs
+++ BlazorStudent/Client/Program.cs
@@ -1,4 +1,5 @@
 using BlazorStudent.Client;
+using CurrieTechnologies.Razor.SweetAlert2;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,5 +8,8 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+builder.Services.AddSweetAlert2();
+//builder.Services.AddSweetAlert2(options => {
+//    options.Theme = SweetAlertTheme.MaterialUI;
+//});
 await builder.Build().RunAsync();
